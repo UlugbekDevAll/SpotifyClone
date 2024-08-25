59b3caa300408337a2a50f2ee39ab82b7e83aadd
UlugbekDevAll <ulugbekbekmuradovall@gmail.com>
2024-08-25 20:45:11 +0500

    Add Choose Mode

--- lib/presentation/choose_mode/pages/choose_mode.dart
+++ lib/presentation/choose_mode/pages/choose_mode.dart
@@ -1,12 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../common/widgets/button/basic_app_button.dart';
+import '../../../core/configs/assets/app_images.dart';
+import '../../../core/configs/assets/app_vectors.dart';
+import '../../../core/configs/theme/app_colors.dart';
 
 class ChooseModePage extends StatelessWidget {
   const ChooseModePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
 
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.fill, image: AssetImage(AppImages.chooseModeBG))),
+          ),
+           Container(
+            color: Colors.black.withOpacity(0.15),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(AppVectors.logo),
+                ),
+                const Spacer(),
+                const Text(
+                  'Choose Mode',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 18),
+                ),
+                const SizedBox(
+                  height: 21,
+                ),
+                const Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis enim purus sed phasellus. Cursus ornare id scelerisque aliquam.',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.grey,
+                      fontSize: 13),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                BasicAppButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                              const ChooseModePage()));
+                    },
+                    title: 'Get Started')
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
